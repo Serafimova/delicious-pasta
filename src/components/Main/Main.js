3e8f032fb9a1f6226ad1470f1b14b85a425f05f9
Milena Serafimova <mdserafimova@gmail.com>
2021-12-20 21:25:23 +0200

    Create Main Page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,3 +1,30 @@
+import { Link } from 'react-router-dom';
+import styles from "./Main.module.css";
+
 export default function Main() {
-  return <h1>main</h1>;
+  return (<>
+    <section className={styles["main-section"]}>
+      <article className="main-content">
+        <p className={styles["main-content-title"]}>
+          Dear Pasta Lover,
+          <br />
+          Welcome to the incredible world of pasta ;)
+        </p>
+        <p>
+          Here you can find your recipies from all around the world for making
+          delicious pasta.
+        </p>
+        <p>Check our <Link className={styles["link"]} to="/recipies">Recipies</Link> for ideas</p>
+      </article>
+        <article className={styles["main-acc"]}>
+        <p>You want to share your favourite pasta recipe with other Pasta lovers?</p>
+          <p className={styles["main-content-text"]}> Feel free to add your's too! Just <Link className={styles["link"]} to="/login">Login</Link></p>
+          <p className={styles["main-content-text"]}>Not Registered?  <Link className={styles["link"]} to="/register">Create an account</Link></p>
+        </article>
+    </section>
+      <section className={styles["recent"]}>
+        <p className={styles["recent-title"]}>Recent recipies</p>
+      </section>
+  </>
+  );
 }
